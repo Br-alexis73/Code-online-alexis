c3c74b67ebefc7ef7f3f2bf75bdca307571d61cb
Bryan Alexis <126200318+Br-alexis73@users.noreply.github.com>
2023-10-05 19:00:03 +0100

    I am committed

---
+++ sampleq3.py
@@ -0,0 +1,17 @@
+def is_palindrome(str):
+    str = str.replace(" ", "").lower()
+    reversed_str = str[::-1]
+
+    for i in range(len(str)):
+        if str[i] != reversed_str[i]:
+            return False
+
+    return True
+
+def main():
+    str = "racecar"
+    result = is_palindrome(str)
+    return print(result)
+
+if __name__ == "__main__":
+    main()
